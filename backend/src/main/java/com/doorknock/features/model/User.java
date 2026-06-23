9f1d9b647b005817da8adb2f7d23162089de20e4
husthunterpy01 <dangminhhust193231@gmail.com>
2026-06-23 23:16:20 +0700

    feat: update users model

---
+++ backend/src/main/java/com/doorknock/features/model/User.java
@@ -0,0 +1,44 @@
+package com.doorknock.features.user.model;
+
+import com.doorknock.features.common.enums.VolunteerRoles;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.Instant;
+import java.util.UUID;
+
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, unique = true)
+    private int phone;
+
+    @Column(nullable = false, unique = true)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String campaign;
+
+    @Column(nullable = false, unique = true)
+    private Instant createdAt;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private VolunteerRoles role;
+}
